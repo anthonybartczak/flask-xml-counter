99051bc698a775f2e7664126dbb9da58ef1760fd
anthonybartczak <to0oni456@gmail.com>
2022-01-22 16:10:40 +0100

    Added get_item_count API

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import xml.etree.ElementTree as ET
+import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
+from io import StringIO
+import csv
+
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def main():
+    return render_template("index.html", )
+
+@app.route("/get_item_count", methods=['GET'])
+def get_item_count():
+    url = request.args.get('url')
+    # test URL "https://drive.google.com/uc?id=1BtVW0P91nobSx2BZsPKKVgz6pVTYw2KA&export=download"
+    # xml = 'https://feeds.skapiec.pl/google-shop-38a3ae44ffe87509ef90a5773c9f1e88.xml'
+    data = urlopen(url).read().decode('ascii','ignore')
+    datafile = StringIO(data)
+    csvReader = csv.reader(datafile)
+    for row in csvReader:
+        xmllink = row[0]
+        url = urllib.request.urlopen(xmllink)
+        data = url.read()
+        datadecoded = (data.decode())
+        pcount = datadecoded.count("</item>")
+        #print(row[0], "liczba produktow to:  ", produkty)
+        #return datadecoded.count("</item>")
+        return {
+            "url": row[0],
+            "count": pcount
+        }
--- calculate.py
+++ calculate.py
@@ -4,14 +4,17 @@ from urllib.request import urlopen
 from io import StringIO
 import csv
 
-data = urlopen("https://drive.google.com/uc?id=1BtVW0P91nobSx2BZsPKKVgz6pVTYw2KA&export=download").read().decode('ascii','ignore')
-datafile = StringIO(data)
-csvReader = csv.reader(datafile)
-for row in csvReader:
-    xmllink = row[0]
- #   xml = 'https://feeds.skapiec.pl/google-shop-38a3ae44ffe87509ef90a5773c9f1e88.xml'
-    url = urllib.request.urlopen(xmllink)
-    data = url.read()
-    datadecoded = (data.decode())
-    produkty = datadecoded.count("</item>")
-    print(row[0], "liczba produktow to:  ", produkty)
+def calculate_item_count(url):
+    # test URL "https://drive.google.com/uc?id=1BtVW0P91nobSx2BZsPKKVgz6pVTYw2KA&export=download"
+    # xml = 'https://feeds.skapiec.pl/google-shop-38a3ae44ffe87509ef90a5773c9f1e88.xml'
+    data = urlopen(url).read().decode('ascii','ignore')
+    datafile = StringIO(data)
+    csvReader = csv.reader(datafile)
+    for row in csvReader:
+        xmllink = row[0]
+        url = urllib.request.urlopen(xmllink)
+        data = url.read()
+        datadecoded = (data.decode())
+        #produkty = datadecoded.count("</item>")
+        #print(row[0], "liczba produktow to:  ", produkty)
+        return datadecoded.count("</item>")
