dfa3de859ab5568a6bfbf8dff73ae597a1830b8a
anthonybartczak <to0oni456@gmail.com>
2022-01-24 09:53:27 +0100

    multiple xml from csv import

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import urllib.request, urllib.parse, urllib.error
 from urllib.request import urlopen
 from io import StringIO
 import csv
+import json
 
 
 app = Flask(__name__)
@@ -14,23 +15,19 @@ def main():
 
 @app.route("/get_item_count", methods=['POST'])
 def get_item_count():
-    print(request.form.get('url'))
     url = request.form.get('url')
-    # test URL "https://drive.google.com/uc?id=1BtVW0P91nobSx2BZsPKKVgz6pVTYw2KA&export=download"
-    # xml = 'https://feeds.skapiec.pl/google-shop-38a3ae44ffe87509ef90a5773c9f1e88.xml'
     data = urlopen(url).read().decode('ascii','ignore')
     datafile = StringIO(data)
     csvReader = csv.reader(datafile)
+    json_data = {}
     for row in csvReader:
         xmllink = row[0]
         url = urllib.request.urlopen(xmllink)
         data = url.read()
         datadecoded = (data.decode())
         pcount = datadecoded.count("</item>")
-        return {
-            "url": row[0],
-            "count": pcount
-        }
+        json_data[row[0]] = pcount
+    return json.dumps(json_data, indent = 4)
 
 if __name__ == '__main__':
     app.run()
